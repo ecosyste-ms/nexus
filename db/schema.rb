c002bbf82b440aef92856cfc5447c2c293b33c7d
Andrew Nesbitt <andrewnez@gmail.com>
2026-01-08 11:30:42 +0000

    Update schema.rb to reflect changes in database structure and versioning

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_31_000003) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_22_185250) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "packages", force: :cascade do |t|
-    t.bigint "repository_id", null: false
-    t.string "name", null: false
-    t.string "group_id", null: false
     t.string "artifact_id", null: false
+    t.datetime "created_at", null: false
+    t.string "group_id", null: false
     t.datetime "last_modified"
     t.jsonb "metadata", default: {}
-    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "repository_id", null: false
     t.datetime "updated_at", null: false
     t.index ["artifact_id"], name: "index_packages_on_artifact_id"
     t.index ["group_id"], name: "index_packages_on_group_id"
@@ -31,29 +31,32 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_31_000003) do
   end
 
   create_table "repositories", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "url", null: false
-    t.string "ecosystem", default: "maven"
-    t.datetime "last_indexed_at"
-    t.string "status", default: "pending"
-    t.text "error_message"
-    t.integer "package_count", default: 0
-    t.bigint "index_size_bytes"
-    t.jsonb "metadata", default: {}
     t.datetime "created_at", null: false
+    t.string "ecosystem", default: "maven"
+    t.text "error_message"
+    t.string "index_chain_id"
+    t.bigint "index_size_bytes"
+    t.string "index_timestamp"
+    t.integer "last_incremental_chunk"
+    t.datetime "last_indexed_at"
+    t.jsonb "metadata", default: {}
+    t.string "name", null: false
+    t.integer "package_count", default: 0
+    t.string "status", default: "pending"
     t.datetime "updated_at", null: false
+    t.string "url", null: false
     t.index ["last_indexed_at"], name: "index_repositories_on_last_indexed_at"
     t.index ["name"], name: "index_repositories_on_name", unique: true
     t.index ["status"], name: "index_repositories_on_status"
   end
 
   create_table "versions", force: :cascade do |t|
-    t.bigint "package_id", null: false
-    t.string "number", null: false
-    t.datetime "last_modified"
-    t.string "packaging"
-    t.jsonb "metadata", default: {}
     t.datetime "created_at", null: false
+    t.datetime "last_modified"
+    t.jsonb "metadata", default: {}
+    t.string "number", null: false
+    t.bigint "package_id", null: false
+    t.string "packaging"
     t.datetime "updated_at", null: false
     t.index ["last_modified"], name: "index_versions_on_last_modified"
     t.index ["package_id", "number"], name: "index_versions_on_package_id_and_number", unique: true
